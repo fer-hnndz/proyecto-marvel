2685ef23bfc67c4c1d21472c84125b95b7a2af0d
fer-hnndz <jfernandohernandez@unitec.edu>
2023-05-30 11:28:01 -0600

    Se crearon get y set para clase Usuario - Tambien se adapto en la clase del sistema de Usuarios

--- src/proyectomarvelstratego/Usuario.java
+++ src/proyectomarvelstratego/Usuario.java
@@ -9,14 +9,45 @@ package proyectomarvelstratego;
  * @author Jorge Hernandez
  */
 public class Usuario {
-    String usuario;
-    String contrasena;
-    int partidas = 0;
+    private String usuario;
+    private String contrasena;
+    private int partidas = 0;
     
     public Usuario(String usuario, String contrasena) {
-        this.usuario = usuario;
-        this.contrasena = contrasena;
+        this.usuario = usuario.trim();
+        this.contrasena = contrasena.trim();
     }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        if (!usuario.trim().isEmpty()) {
+            this.usuario = usuario.trim();
+        }
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        if (!contrasena.trim().isEmpty()) {
+            this.contrasena = contrasena.trim();        
+        }
+    }
+    
+    /**
+     * Verifica que el nombre de usuario y la contraseña especificada sea igual a a guardada por el usuario.
+     * @return 
+     */
+    public boolean validarCredenciales(String usuario, String contrasena) {
+        return (this.usuario.equals(usuario) && this.contrasena.equals(contrasena));
+    }
+    
+    
+    
     
     
 }
